eb4c148099da4ca9182ce312cfd7da07c0a66345
shivanikedare-del <shivani.kedare@gmail.com>
2026-01-12 18:04:44 +0000

    data types summary

---
+++ 01_basics/03_datatypes-summary.js
@@ -0,0 +1,29 @@
+// Primitive Datatypes
+// 7 types: String, Number, Boolean, null, undefined, BigInt, Symbol
+
+// Reference (Non Primitive) Datatypes
+// Array, Objects, Functions
+
+
+let someValue; // undefined
+
+let id = Symbol('123')
+let id2 = Symbol('123')
+
+console.log(id === id2);
+
+const bigValue = 54347138727495435666n // BigInt
+
+// Array
+const arr1 = ["1","2","3"]
+
+// Object
+let myObj = 
+{
+    naam : "Shivani",
+    age : 26
+}
+
+
+// datatype for all non primitive is function
+// but specifically for Functions it is called 'object function'
