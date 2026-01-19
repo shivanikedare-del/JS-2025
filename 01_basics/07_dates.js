783f61ec6457ed2ea6efb9ca17d038f137f43cdd
shivanikedare-del <shivani.kedare@gmail.com>
2026-01-19 18:15:12 +0000

    dates-2

--- 01_basics/07_dates.js
+++ 01_basics/07_dates.js
@@ -13,3 +13,17 @@ console.log(myNewDate.toLocaleString());
 
 let anotherDate = new Date("2025-01-18")               // for string month will start from 1
 console.log(anotherDate.toDateString());
+
+let myTime = Date.now()
+console.log(myTime);
+
+console.log(anotherDate.getTime());
+
+console.log(Math.floor(Date.now()/1000));
+
+console.log(myDate.getDay());
+console.log(myDate.getMonth());
+
+console.log(myDate.toLocaleString('default', {
+    weekday: 'long'
+}))
