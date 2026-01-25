83bbf2249c278bf0b4090e6538bf902b4ccaf01b
shivanikedare-del <shivani.kedare@gmail.com>
2026-01-25 18:07:40 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,16 @@
+let arr1 = new Array(1,2,3,4,5)
+
+console.log(arr1);
+console.log(arr1.length);
+console.log(arr1.includes(2));
+console.log(arr1.indexOf(1));
+
+// arr1.push(10)
+// console.log(arr1);
+// arr1.pop()
+// console.log(arr1);
+
+arr1.unshift(11)
+console.log(arr1);
+arr1.shift()
+console.log(arr1);
