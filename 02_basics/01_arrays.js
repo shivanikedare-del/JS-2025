a0801e540707b479b18ea9291adc512713939180
shivanikedare-del <shivani.kedare@gmail.com>
2026-01-27 08:39:48 +0000

    s(p)lice

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -5,12 +5,17 @@ console.log(arr1.length);
 console.log(arr1.includes(2));
 console.log(arr1.indexOf(1));
 
-// arr1.push(10)
-// console.log(arr1);
-// arr1.pop()
-// console.log(arr1);
-
-arr1.unshift(11)
+arr1.push(10)               // will add 10 at the end
 console.log(arr1);
-arr1.shift()
+arr1.pop()                  // removes last element
+console.log(arr1);
+
+arr1.unshift(11)            // will add 11 at start
+console.log(arr1);
+arr1.shift()                // removes first element
+console.log(arr1);
+
+console.log(arr1.slice(2,4));   // only gives the asked part as result
+console.log(arr1);
+console.log(arr1.splice(2,4));  // gives asked part as result & also removes it from original array
 console.log(arr1);
