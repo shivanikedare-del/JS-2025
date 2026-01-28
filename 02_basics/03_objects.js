dcc825a8dfaea2a5c5be5a125a71c542a0957fdd
shivanikedare-del <shivani.kedare@gmail.com>
2026-01-28 12:37:11 +0000

    object

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -2,7 +2,23 @@
 // Object.create
 
 // object literals
+
+const mySym = Symbol("Sym1")
+
 const JsUser = {
-    name: "Shivani"
-    
+    name: "Shivani",
+    email: "shivani@gmail.com",
+    [mySym]: "key"
 }
+
+console.log(`Hello JS user`);
+console.log(JsUser["email"]);
+
+JsUser.email = "kedare@gmail.com"
+console.log(JsUser);
+
+JsUser.greeting = function() {
+    console.log(`Welcome to JS, ${JsUser.name}`);
+}
+
+console.log(JsUser.greeting());
