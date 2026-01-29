035c7e19291f8d2b2de4706df88d3f6b283c8f71
shivanikedare-del <shivani.kedare@gmail.com>
2026-01-29 10:21:24 +0000

    objects

---
+++ 02_basics/04_object.js
@@ -0,0 +1,37 @@
+// const user = new Object()
+const user = {}
+
+user.name = "Shivani"
+user.age = 26
+user.mail = "some@mail.com"
+
+console.log(user);
+
+const newUser = {
+    name : {
+        fullName: {
+            firstName: "User",
+            lastName: "Id"
+        }
+    },
+    age : 22
+}
+
+console.log(newUser.name.fullName.firstName);
+
+const obj1 = { a: "one", b: "two" }
+const obj2 = { c: "three", d: "four" }
+
+obj3 = {obj1, obj2}
+
+console.log(obj3);
+
+console.log(Object.assign(obj1, obj2));
+
+console.log({...obj1, ...obj2});
+
+console.log(Object.keys(user));
+console.log(Object.entries(user));
+console.log(Object.values(user));
+
+console.log(Object.hasOwnProperty('name'));
