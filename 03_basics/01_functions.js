129be7e098363b174e4d5e96590fa89e675bb3b5
shivanikedare-del <shivani.kedare@gmail.com>
2026-02-03 08:05:26 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -1,7 +1,34 @@
 // function is a packet of code
 
 function addTwo(num1,num2){
-    console.log(num1 + num2)
+    // console.log(num1 + num2)
+    return num1 + num2
+}
+console.log(addTwo(1,9))
+
+
+function username(name){
+    if(!name){
+        console.log(`Enter a valid username`);
+        return
+    }
+    return `Welcome ${name}!`
+}
+console.log(username());
+
+
+function numbers(num,...num1){
+    return num1
+}
+console.log(numbers(1,2,3,4));
+
+
+const productDetails = {
+    product: "Pack of 10",
+    price: "149"
 }
 
-const result = addTwo(1,9)
+function handleObject(anyobject){
+    return `${anyobject.product} will cost ${anyobject.price}`
+}
+console.log(handleObject(productDetails));
